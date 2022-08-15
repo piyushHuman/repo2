3d4100f8825d078636481872c9c09e4ef0ace541
piyush <piyushsujeethkumar944@gmail.com>
2022-08-15 09:49:33 +0530

    hit commit

---
+++ src/firstclass.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class firstclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","E:\\chrome diver\\chromedriver.exe"); 
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://facebook.com");
+	driver.findElement(By.xpath("//input[@data-testid='royal_email']")).sendKeys("xyz@gmail.com");
+		driver.findElement(By.cssSelector("input[placeholder = 'Password']")).sendKeys("qwerty");
+		driver.findElement(By.cssSelector("button[data-testid = 'royal_login_button']")).click();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
